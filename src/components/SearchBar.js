a2753e035831933b4c4fc378debaacf141c5579a
quangloc.nguyen <quangloc.nguyen>
2020-02-03 17:37:32 +0700

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-class SearchBar extends React.Component{
-    render(){
-        return(
-            <div>SearchBar</div>
+class SearchBar extends React.Component {
+    state = {
+        term: ''
+    }
+    render() {
+        return (
+            <div className='ui segment search-bar'>
+                <form className='ui form'>
+                    <div className='ui segment'>
+
+                    <label>First Name</label>
+                    <input
+                        type='text'
+                        value={this.state.term}
+                        onChange={(event) => this.setState({ term: event.target.value })}
+                        />
+
+                        </div>
+                </form>
+            </div>
 
         )
     }

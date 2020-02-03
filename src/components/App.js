1e7e61395964dc3f2791546ea824d2c85a72e7e9
quangloc.nguyen <quangloc.nguyen>
2020-02-03 11:28:50 +0700

    first

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <i class="sg flag"></i>
+            </div>
+
+        )
+    }
+
+}
+
+export default App
